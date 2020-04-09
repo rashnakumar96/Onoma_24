82ac9cd8980d07237ddcdbdcfae923e2126a3940
alexthemonk <liushucheng9661@gmail.com>
2020-04-09 08:35:48 -0500

    scripts building for Windows and Linux

--- build.go
+++ build.go
@@ -1,8 +1,8 @@
 package main
 
 //go:generate go build -o bin/namehelp namehelp
-//go:generate go build -o Sub-Rosa/src/bin/MacOS/namehelp namehelp
 //go:generate go build -o bin/Sub-Rosa
+//go:generate env GOOS=darwin GOARCH=amd64 go build -o Sub-Rosa/src/bin/MacOS/namehelp namehelp
 //go:generate env GOOS=windows GOARCH=amd64 go build -o Sub-Rosa/src/bin/Windows/namehelp namehelp
 //go:generate env GOOS=linux GOARCH=amd64 go build -o Sub-Rosa/src/bin/Linux/namehelp namehelp
 
