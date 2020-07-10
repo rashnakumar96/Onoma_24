b8c0ccaf64ad2bbe961d3ab025ccaaa474fb6f89
alexthemonk <liushucheng9661@gmail.com>
2020-07-10 14:13:00 -0500

    reformat find_cdn

---
+++ analysis/utils.py
@@ -0,0 +1,18 @@
+import tldextract, json
+
+# Convert a url (string) to the domain name (string) of the index page
+def url_to_domain(url):
+    ext = tldextract.extract(url)
+    return ".".join(ext)
+
+# Load file into a json object
+def load_json(fn):
+    data = None
+    with open(fn, 'r') as fp:
+        data = json.load(fp)
+    return data
+
+# Dump a json object into a file
+def dump_json(data, fn):
+    with open(fn, 'w') as fp:
+        json.dump(data, fp)
